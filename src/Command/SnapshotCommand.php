25855a6eb25b9866646c975bfa193f37d0cde03d
Chris Harvey <chris@chrisnharvey.com>
2019-11-16 13:55:47 +0000

    Add the ability to purge old snapshots when creating a new snapshot

--- src/Command/SnapshotCommand.php
+++ src/Command/SnapshotCommand.php
@@ -5,6 +5,8 @@ namespace App\Command;
 use App\Snapshot;
 use App\SnapshotFinder;
 use Encore\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -27,6 +29,13 @@ class SnapshotCommand extends Command
     {
         $this
             ->setName('snapshot')
+            ->addOption(
+                'purge-older-than',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'How old should a snapshot be (in days) to be purged?',
+                null
+            )
             ->setDescription('Take a snapshot of the current system state');
     }
 
@@ -35,6 +44,14 @@ class SnapshotCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if ($purgeOlderThan = $input->getOption('purge-older-than')) {
+            $command = $this->getApplication()->find('purge');
+
+            $command->run(new ArrayInput([
+                '--older-than' => $purgeOlderThan
+            ]), $output);
+        }
+
         $snapshot = Snapshot::create();
 
         $output->writeln("<fg=green>Snapshot created at {$snapshot->getPath()}</>");
